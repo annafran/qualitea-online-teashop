9de1bfe258b4e67011303119529f76d7529c9046
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-03-29 22:16:05 +1300

    add primary brand colours as root variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = ({ logo, headerImage }) => {
                     <img className="logo" alt="logo" src={logo} />
                 </div>
                 <div className="companyNameContainer">
-                    <h1 className="companyName">Quali-Tea</h1>
+                    <h1 className="companyName">QualiTea</h1>
                 </div>
             </div>
             <div className="imageBannerContainer"></div>
