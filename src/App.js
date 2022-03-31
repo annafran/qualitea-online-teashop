1a6a9a1323bad37343bba010a1c311c0d6cea24b
Anna Munro <anna.munro@developersinstitute.co.nz>
2022-03-31 13:46:58 +1300

    clean unused classes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { React, useEffect, useState } from "react";
 import "./App.css";
-import headerImage from "./images/headerImage.jpg";
 import logo from "./images/transparentLogo.png";
 import Products from "./components/Products";
 import Header from "./components/Header";
@@ -8,13 +7,9 @@ import FilterBy from "./components/FilterBy";
 import SortBy from "./components/SortBy";
 import filterByCategory from "./utils/filterByCategory";
 import sortProducts from "./utils/sortProducts";
-
-// The function that makes the fetch request to the Products API
 import { getProducts } from "./services/getProducts";
 
 function App() {
-  // use the products variable to read all of your products
-  // and display them on your page
   const [products, setProducts] = useState([]);
   const [filterBy, setFilterBy] = useState("all");
   const [sortBy, setSortBy] = useState("low");
@@ -39,8 +34,8 @@ function App() {
   }, [filterBy, sortBy, products]);
 
   return (
-    <div className="container">
-      <Header logo={logo} headerImage={headerImage} />
+    <div>
+      <Header logo={logo} />
       <div className="toolbar">
         <FilterBy setFilterBy={setFilterBy} />
         <SortBy setSortBy={setSortBy} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({ logo, headerImage }) => {
   return (
-    <div className="header">
+    <div>
       <div className="brandingContainer">
         <div className="logoContainer">
           <img className="logo" alt="logo" src={logo} />
